09618f06d3a42be9564c396a8873951636d03b86
bchae <cbr5964@naver.com>
2020-04-25 16:04:38 +0900

    Update output.c

--- P1/output.c
+++ P1/output.c
@@ -145,7 +145,7 @@ void output(){
 				if(buf.num == -1 || buf.num == 0 || buf.num == 1){
 					dot_out(buf.num);
 				}
-				else if(strcpy(buf.text, "") != 0){
+				else if(strcmp(buf.text, "") != 0){
 					dot_draw(buf.text);
 				}
 			}
