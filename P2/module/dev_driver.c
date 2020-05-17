ad036f602a4ffbd1d241db2182e8b082cb785642
b-chae <54340317+b-chae@users.noreply.github.com>
2020-05-17 12:13:14 +0900

    enable dot device (#5)
    * 주석 추가 ><
    
    * 세부조건 추가
    
    * Create Document.docx
    
    * warning 문제 해결
    
    * rename folder
    
    * change func name
    
    * add dot device (test)

--- P2/module/dev_driver.c
+++ P2/module/dev_driver.c
@@ -19,62 +19,93 @@ AUTH : largest@huins.com */
 #include <linux/version.h>
 
 
-#define IOM_FND_MAJOR 242		// ioboard fpga device major number
-#define IOM_FND_NAME "dev_driver"		// ioboard fpga device name
+#define IOM_DEVICE_MAJOR 242		// ioboard fpga device major number
+#define IOM_DEVICE_NAME "dev_driver"		// ioboard fpga device name
 
 #define IOM_FND_ADDRESS 0x08000004 // pysical address
 #define IOM_LED_ADDRESS 0x08000016 // pysical address
+#define IOM_FPGA_DOT_ADDRESS 0x08000210
 
 //Global variable
 static int fpga_fnd_port_usage = 0;
 static unsigned char *iom_fpga_fnd_addr;
 static int ledport_usage = 0;
 static unsigned char *iom_fpga_led_addr;
+static int fpga_dot_port_usage = 0;
+static unsigned char *iom_fpga_dot_addr;
+
+unsigned char fpga_number[10][10] = {
+	{0x3e,0x7f,0x63,0x73,0x73,0x6f,0x67,0x63,0x7f,0x3e}, // 0
+	{0x0c,0x1c,0x1c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x1e}, // 1
+	{0x7e,0x7f,0x03,0x03,0x3f,0x7e,0x60,0x60,0x7f,0x7f}, // 2
+	{0xfe,0x7f,0x03,0x03,0x7f,0x7f,0x03,0x03,0x7f,0x7e}, // 3
+	{0x66,0x66,0x66,0x66,0x66,0x66,0x7f,0x7f,0x06,0x06}, // 4
+	{0x7f,0x7f,0x60,0x60,0x7e,0x7f,0x03,0x03,0x7f,0x7e}, // 5
+	{0x60,0x60,0x60,0x60,0x7e,0x7f,0x63,0x63,0x7f,0x3e}, // 6
+	{0x7f,0x7f,0x63,0x63,0x03,0x03,0x03,0x03,0x03,0x03}, // 7
+	{0x3e,0x7f,0x63,0x63,0x7f,0x7f,0x63,0x63,0x7f,0x3e}, // 8
+	{0x3e,0x7f,0x63,0x63,0x7f,0x3f,0x03,0x03,0x03,0x03} // 9
+};
+
+unsigned char fpga_set_full[10] = {
+	// memset(array,0x7e,sizeof(array));
+	0x7f,0x7f,0x7f,0x7f,0x7f,0x7f,0x7f,0x7f,0x7f,0x7f
+};
+
+unsigned char fpga_set_blank[10] = {
+	// memset(array,0x00,sizeof(array));
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+};
+
 
 // define functions...
-ssize_t iom_fpga_fnd_write(struct file *inode, const char *gdata, size_t length, loff_t *off_what);
-ssize_t iom_fpga_fnd_read(struct file *inode, char *gdata, size_t length, loff_t *off_what);
-int iom_fpga_fnd_open(struct inode *minode, struct file *mfile);
-int iom_fpga_fnd_release(struct inode *minode, struct file *mfile);
+ssize_t iom_device_write(struct file *inode, const char *gdata, size_t length, loff_t *off_what);
+ssize_t iom_device_read(struct file *inode, char *gdata, size_t length, loff_t *off_what);
+int iom_device_open(struct inode *minode, struct file *mfile);
+int iom_device_release(struct inode *minode, struct file *mfile);
 
 // define file_operations structure 
-struct file_operations iom_fpga_fnd_fops =
+struct file_operations iom_device_fops =
 {
 	.owner		=	THIS_MODULE,
-	.open		=	iom_fpga_fnd_open,
-	.write		=	iom_fpga_fnd_write,	
-	.read		=	iom_fpga_fnd_read,	
-	.release	=	iom_fpga_fnd_release,
+	.open		=	iom_device_open,
+	.write		=	iom_device_write,	
+	.read		=	iom_device_read,	
+	.release	=	iom_device_write,
 };
 
 // when fnd device open ,call this function
-int iom_fpga_fnd_open(struct inode *minode, struct file *mfile) 
+int iom_device_open(struct inode *minode, struct file *mfile) 
 {	
 	if(fpga_fnd_port_usage != 0) return -EBUSY;
 	if(ledport_usage != 0) return -EBUSY;
+	if(fpga_dot_port_usage != 0) return -EBUSY;
 
 	fpga_fnd_port_usage = 1;
 	ledport_usage = 1;
+	fpga_dot_port_usage = 1;
 
 	return 0;
 }
 
 // when fnd device close ,call this function
-int iom_fpga_fnd_release(struct inode *minode, struct file *mfile) 
+int iom_device_release(struct inode *minode, struct file *mfile) 
 {
 	fpga_fnd_port_usage = 0;
 	ledport_usage = 0;
+	fpga_dot_port_usage = 0;
 
 	return 0;
 }
 
 // when write to fnd device  ,call this function
-ssize_t iom_fpga_fnd_write(struct file *inode, const char *gdata, size_t length, loff_t *off_what) 
+ssize_t iom_device_write(struct file *inode, const char *gdata, size_t length, loff_t *off_what) 
 {
 	int i;
 	unsigned char value[4];
 	unsigned short int value_short = 0;
 	unsigned short _s_value = 0;
+	unsigned short int tmp_value;
 	const char *tmp = gdata;
 
 	if (copy_from_user(&value, tmp, 4))
@@ -84,6 +115,9 @@ ssize_t iom_fpga_fnd_write(struct file *inode, const char *gdata, size_t length,
     if(value[0] != 0){
 		if(value[0] == 1){
 			_s_value = 128;
+			for(i=0; i<10; i++){
+				outw(fpga_number[1][i], (unsigned int)iom_fpga_dot_addr + i*2);
+			}
 		}
 		else if(value[0] == 2){
 			_s_value = 64;
@@ -115,7 +149,7 @@ ssize_t iom_fpga_fnd_write(struct file *inode, const char *gdata, size_t length,
 }
 
 // when read to fnd device  ,call this function
-ssize_t iom_fpga_fnd_read(struct file *inode, char *gdata, size_t length, loff_t *off_what) 
+ssize_t iom_device_read(struct file *inode, char *gdata, size_t length, loff_t *off_what) 
 {
 	int i;
 	unsigned char value[4];
@@ -134,11 +168,11 @@ ssize_t iom_fpga_fnd_read(struct file *inode, char *gdata, size_t length, loff_t
 	return length;
 }
 
-int __init iom_fpga_fnd_init(void)
+int __init iom_device_init(void)
 {
 	int result;
 
-	result = register_chrdev(IOM_FND_MAJOR, IOM_FND_NAME, &iom_fpga_fnd_fops);
+	result = register_chrdev(IOM_DEVICE_MAJOR, IOM_DEVICE_NAME, &iom_fpga_fnd_fops);
 	if(result < 0) {
 		printk(KERN_WARNING"Can't get any major\n");
 		return result;
@@ -147,19 +181,20 @@ int __init iom_fpga_fnd_init(void)
 	iom_fpga_fnd_addr = ioremap(IOM_FND_ADDRESS, 0x4);
 	iom_fpga_led_addr = ioremap(IOM_LED_ADDRESS, 0x1);
 
-	printk("init module, %s major number : %d\n", IOM_FND_NAME, IOM_FND_MAJOR);
+	printk("init module, %s major number : %d\n", IOM_DEVICE_NAME, IOM_DEVICE_MAJOR);
 
 	return 0;
 }
 
-void __exit iom_fpga_fnd_exit(void) 
+void __exit iom_device_exit(void) 
 {
 	iounmap(iom_fpga_fnd_addr);
-	unregister_chrdev(IOM_FND_MAJOR, IOM_FND_NAME);
+	iounmap(iom_fpga_led_addr);
+	unregister_chrdev(IOM_DEVICE_MAJOR, IOM_DEVICE_NAME);
 }
 
-module_init(iom_fpga_fnd_init);
-module_exit(iom_fpga_fnd_exit);
+module_init(iom_device_init);
+module_exit(iom_device_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Huins");
