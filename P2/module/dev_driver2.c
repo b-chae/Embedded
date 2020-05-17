72d33fa43ad9294f35ab9bafc010c8c34c24d7cb
bchae <cbr5964@naver.com>
2020-05-17 22:06:44 +0900

    Update dev_driver2.c

--- P2/module/dev_driver2.c
+++ P2/module/dev_driver2.c
@@ -134,6 +134,9 @@ void deal_with_data(void){
 	}
 	
 	fnd_write(value);
+	dot_write(real_value);
+	led_write(real_value);
+	text_write(0,0);
 }
 
 ssize_t iom_device_write(struct file *inode, const char *gdata, size_t length, loff_t *off_what) {
@@ -157,8 +160,6 @@ ssize_t iom_device_write(struct file *inode, const char *gdata, size_t length, l
 	mydata.timer.data = (unsigned long)&mydata;
 	mydata.timer.function = kernel_timer_blink;
 	
-	fnd_wirte("1234");
-
 	add_timer(&mydata.timer);
 	return 1;
 }
@@ -167,6 +168,66 @@ void fnd_write(const char* value){
 	unsigned short int value_short;
 	value_short = value[0] << 12 | value[1] << 8 |value[2] << 4 |value[3];
 	outw(value_short,(unsigned int)iom_fpga_fnd_addr);
+}
+
+void dot_write(int n){
+	int i;
+	for(i=0; i<10; i++){
+		outw(fpga_number[n][i], (unsigned int)iom_fpga_dot_addr + 2*i);
+	}
+}
+
+void led_write(unsigned char n){
+	
+	unsigned short _s_value = 0;
+	
+	switch(n){
+		case 1 : _s_value = 128; break;
+		case 2 : _s_value = 64; break;
+		case 3 : _s_value = 32; break;
+		case 4 : _s_value = 16; break;
+		case 5 : _s_value = 8; break;
+		case 6 : _s_value = 4; break;
+		case 7 : _s_value = 2; break;
+		case 8 : _s_value = 1; break;
+	}
+
+	outw(_s_value, (unsigned int)iom_fpga_led_addr);
+}
+
+void text_write(int l_index, int r_index) 
+{
+	int i;
+
+	unsigned char value[32];
+   	unsigned short int _s_value = 0;
+	
+	for(i=0; i<l_index; i++){
+		value[i] = ' ';
+	}
+	for(i=l_index; i<l_index+STRLEN_STUDENT_NUMBER; i++){
+		value[i] = student_number[i-l_index];
+	}
+	for(i=i; i<16; i++){
+		value[i] = ' ';
+	}
+	
+	for(i=16; i<r_index+16; i++){
+		value[i] = ' ';
+	}
+	for(i=i; i<r_index+16+STRLEN_MY_NAME; i++){
+		value[i] = my_name[i-r_index-16];
+	}
+	for(i=i; i<32; i++){
+		value[i] = ' ';
+	}
+
+	for(i=0;i<32;i++)
+    	{
+        _s_value = (value[i] & 0xFF) << 8 | value[i + 1] & 0xFF;
+	outw(_s_value,(unsigned int)iom_fpga_text_lcd_addr+i);
+        i++;
+    	}
 }
 
 int __init iom_device_init(void)
