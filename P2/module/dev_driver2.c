3ec8f04cdbf5d7689a12073e27d31a7fb682c826
bchae <cbr5964@naver.com>
2020-05-17 21:15:53 +0900

    Update dev_driver2.c

--- P2/module/dev_driver2.c
+++ P2/module/dev_driver2.c
@@ -5,21 +5,12 @@
 #include <linux/kernel.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
-
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/init.h>
-#include <linux/slab.h>
-#include <linux/platform_device.h>
 #include <linux/delay.h>
 
 #include <asm/io.h>
 #include <asm/uaccess.h>
 #include <linux/kernel.h>
 #include <linux/ioport.h>
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/init.h>
 #include <linux/version.h>
 
 #define IOM_DEVICE_MAJOR 242		// ioboard fpga device major number
@@ -35,43 +26,89 @@
 
 #define KERNEL_TIMER_NAME "dev_driver2"
 
-static int kernel_timer_usage = 0;
+//Global variable
+static int device_usage = 0;
+static unsigned char *iom_fpga_fnd_addr;
+static unsigned char *iom_fpga_led_addr;
+static unsigned char *iom_fpga_dot_addr;
+static unsigned char *iom_fpga_text_lcd_addr;
 
-int kernel_timer_open(struct inode *, struct file *);
-int kernel_timer_release(struct inode *, struct file *);
-ssize_t kernel_timer_write(struct file *, const char *, size_t, loff_t *);
+const char* student_number = "20171696";
+const char* my_name = "byeori chae";
 
-static struct file_operations kernel_timer_fops =
-{ .open = kernel_timer_open, .write = kernel_timer_write,
-	.release = kernel_timer_release };
-
-static struct struct_mydata {
-	struct timer_list timer;
-	int count;
+unsigned char fpga_number[10][10] = {
+	{0x3e,0x7f,0x63,0x73,0x73,0x6f,0x67,0x63,0x7f,0x3e}, // 0
+	{0x0c,0x1c,0x1c,0x0c,0x0c,0x0c,0x0c,0x0c,0x0c,0x1e}, // 1
+	{0x7e,0x7f,0x03,0x03,0x3f,0x7e,0x60,0x60,0x7f,0x7f}, // 2
+	{0xfe,0x7f,0x03,0x03,0x7f,0x7f,0x03,0x03,0x7f,0x7e}, // 3
+	{0x66,0x66,0x66,0x66,0x66,0x66,0x7f,0x7f,0x06,0x06}, // 4
+	{0x7f,0x7f,0x60,0x60,0x7e,0x7f,0x03,0x03,0x7f,0x7e}, // 5
+	{0x60,0x60,0x60,0x60,0x7e,0x7f,0x63,0x63,0x7f,0x3e}, // 6
+	{0x7f,0x7f,0x63,0x63,0x03,0x03,0x03,0x03,0x03,0x03}, // 7
+	{0x3e,0x7f,0x63,0x63,0x7f,0x7f,0x63,0x63,0x7f,0x3e}, // 8
+	{0x3e,0x7f,0x63,0x63,0x7f,0x3f,0x03,0x03,0x03,0x03} // 9
 };
 
-struct mydata{
+unsigned char fpga_set_full[10] = {
+	// memset(array,0x7e,sizeof(array));
+	0x7f,0x7f,0x7f,0x7f,0x7f,0x7f,0x7f,0x7f,0x7f,0x7f
+};
+
+unsigned char fpga_set_blank[10] = {
+	// memset(array,0x00,sizeof(array));
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+};
+
+// define functions...
+ssize_t iom_device_write(struct file *inode, const char *gdata, size_t length, loff_t *off_what);
+ssize_t iom_device_read(struct file *inode, char *gdata, size_t length, loff_t *off_what);
+int iom_device_open(struct inode *minode, struct file *mfile);
+int iom_device_release(struct inode *minode, struct file *mfile);
+void fnd_write(const char* value);
+void dot_write(int n);
+void led_write(unsigned char n);
+void text_write(int, int);
+
+// define file_operations structure 
+static struct file_operations iom_device_fops =
+{
+	.owner		=	THIS_MODULE,
+	.open		=	iom_device_open,
+	.write		=	iom_device_write,	
+	.read		=	iom_device_read,	
+	.release	=	iom_device_release
+};
+
+static struct mydata{
 	int timer_interval;
 	int timer_count;
 	int timer_init;
 };
 
-struct struct_mydata mydata;
+static struct struct_timer_data{
+	struct timer_list timer;
+	int count;
+};
+
 struct mydata option;
+struct struct_timer_data timer_data;
 int major;
 
-int kernel_timer_release(struct inode *minode, struct file *mfile) {
-	printk("kernel_timer_release\n");
-	kernel_timer_usage = 0;
+// when fnd device close ,call this function
+int iom_device_release(struct inode *minode, struct file *mfile) 
+{
+	device_usage = 0;
+
 	return 0;
 }
 
-int kernel_timer_open(struct inode *minode, struct file *mfile) {
-	printk("kernel_timer_open\n");
-	if (kernel_timer_usage != 0) {
-		return -EBUSY;
-	}
-	kernel_timer_usage = 1;
+// when fnd device open ,call this function
+int iom_device_open(struct inode *minode, struct file *mfile) 
+{	
+	if(device_usage != 0) return -EBUSY;
+	
+	device_usage = 1;
+
 	return 0;
 }
 
@@ -91,15 +128,40 @@ static void kernel_timer_blink(unsigned long timeout) {
 	add_timer(&mydata.timer);
 }
 
-ssize_t kernel_timer_write(struct file *inode, const char *gdata, size_t length, loff_t *off_what) {
+ssize_t iom_device_write(struct file *inode, const char *gdata, size_t length, loff_t *off_what) {
 	const char *tmp = gdata;
 	char kernel_timer_buff = 0;
+	unsigned char value[4];
+	unsigned char real_value = 0;
 
 	printk("write\n");
 	// 1 byte
 	if (copy_from_user(&option, gdata, sizeof(option))) {
 		return -EFAULT;
 	}
+	
+	value[0] = option.timer_init/1000;
+	value[1] = option.timer_init/100%10;
+	value[2] = option.timer_init%100/10;
+	value[3] = option.timer_init%10;
+	
+	fnd_write(value);
+	
+	if(value[0] != 0){
+		real_value = value[0];
+	}
+	else if(value[1] != 0){
+		real_value = value[1];
+	}
+	else if(value[2] != 0){
+		real_value = value[2];
+	}
+	else if(value[3] != 0){
+		real_value = value[3];
+	
+	dot_write(real_value);
+	led_write(real_value);
+	text_write(0, 0);
 
 	mydata.count = 0;
 
@@ -115,7 +177,73 @@ ssize_t kernel_timer_write(struct file *inode, const char *gdata, size_t length,
 	return 1;
 }
 
-int __init kernel_timer_init(void)
+void fnd_write(const char* value){
+	unsigned short int value_short;
+	value_short = value[0] << 12 | value[1] << 8 |value[2] << 4 |value[3];
+	outw(value_short,(unsigned int)iom_fpga_fnd_addr);
+}
+
+void dot_write(int n){
+	int i;
+	for(i=0; i<10; i++){
+		outw(fpga_number[n][i], (unsigned int)iom_fpga_dot_addr + 2*i);
+	}
+}
+
+void led_write(unsigned char n){
+	
+	unsigned short _s_value = 0;
+	
+	switch(n){
+		case 1 : _s_value = 128; break;
+		case 2 : _s_value = 64; break;
+		case 3 : _s_value = 32; break;
+		case 4 : _s_value = 16; break;
+		case 5 : _s_value = 8; break;
+		case 6 : _s_value = 4; break;
+		case 7 : _s_value = 2; break;
+		case 8 : _s_value = 1; break;
+	}
+
+	outw(_s_value, (unsigned int)iom_fpga_led_addr);
+}
+
+void text_write(int l_index, int r_index) 
+{
+	int i;
+
+	unsigned char value[32];
+   	unsigned short int _s_value = 0;
+	
+	for(i=0; i<l_index; i++){
+		value[i] = ' ';
+	}
+	for(i=l_index; i<l_index+STRLEN_STUDENT_NUMBER; i++){
+		value[i] = student_number[i-l_index];
+	}
+	for(i=i; i<16; i++){
+		value[i] = ' ';
+	}
+	
+	for(i=16; i<r_index+16; i++){
+		value[i] = ' ';
+	}
+	for(i=i; i<r_index+16+STRLEN_MY_NAME; i++){
+		value[i] = my_name[i-r_index-16];
+	}
+	for(i=i; i<32; i++){
+		value[i] = ' ';
+	}
+
+	for(i=0;i<32;i++)
+    	{
+        _s_value = (value[i] & 0xFF) << 8 | value[i + 1] & 0xFF;
+	outw(_s_value,(unsigned int)iom_fpga_text_lcd_addr+i);
+        i++;
+    	}
+}
+
+int __init iom_device_init(void)
 {
 	
 	printk("kernel_timer_init\n");
@@ -133,7 +261,7 @@ int __init kernel_timer_init(void)
 	return 0;
 }
 
-void __exit kernel_timer_exit(void)
+void __exit iom_device_exit(void)
 {
 	printk("kernel_timer_exit\n");
 	kernel_timer_usage = 0;
@@ -142,8 +270,8 @@ void __exit kernel_timer_exit(void)
 	unregister_chrdev(major, KERNEL_TIMER_NAME);
 }
 
-module_init(kernel_timer_init);
-module_exit( kernel_timer_exit);
+module_init(iom_device_init);
+module_exit( iom_device_exit);
 
 MODULE_LICENSE ("GPL");
 MODULE_AUTHOR ("author");
