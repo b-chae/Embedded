5c436125c37c6e7640b22adf9f311490820b2476
b-chae <cbr5964@naver.com>
2020-05-21 20:41:25 +0900

    complete ioctl test

--- P2/module/dev_driver.c
+++ P2/module/dev_driver.c
@@ -16,9 +16,9 @@ int iom_device_open(struct inode *minode, struct file *mfile) {
 	return 0;
 }
 
-int iom_device_ioctl(struct inode *minode, struct file *mfile, unsigned int cmd, unsigned long arg){
+static long iom_device_ioctl(struct file *mfile, unsigned int cmd, unsigned long arg){
 	int ret;
-	mydata param;
+	struct mydata param;
 	
 	switch(cmd){
 		case IOCTL_SEND_ARG:
